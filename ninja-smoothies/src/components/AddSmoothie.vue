7c317d69eeed49ae8b801c32cef0844ae42a0d54
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-05-18 21:02:43 +0300

    Update ninja-smoothies l-52

--- ninja-smoothies/src/components/AddSmoothie.vue
+++ ninja-smoothies/src/components/AddSmoothie.vue
@@ -6,9 +6,10 @@
         <label for="title">Smoothie Title</label>
         <input type="text" name="title" v-model="title" />
       </div>
-      <div v-for="(ing, index) in ingredients" :key="index">
+      <div v-for="(ing, index) in ingredients" :key="index" class="field">
         <label for="ingredient">ingredient:</label>
-        <input type="text" name="ingredient" v-model="ingredients[index]">
+        <input type="text" name="ingredient" v-model="ingredients[index]" />
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient">Add an ingredient</label>
@@ -23,6 +24,9 @@
 </template>
 
 <script>
+import db from "@/firebase/init";
+import slugify from 'slugify'
+
 export default {
   name: "AddSmoothie",
   data() {
@@ -30,12 +34,31 @@ export default {
       title: null,
       another: null,
       ingredients: [],
-      feedback: null
+      feedback: null,
+      slug: null
     };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title, this.ingredients);
+      if (this.title) {
+        this.feedback = null;
+        this.slug = slugify(this.title, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+        db.collection("smoothies").add({
+          title: this.title,
+          ingredients: this.ingredients,
+          slug: this.slug
+        }).then(() => {
+          this.$router.push({name: 'Index'})
+        }).catch(err => {
+          console.log(err);
+        })
+      } else {
+        this.feedback = "You must enter a smoothie title";
+      }
     },
     addIng() {
       if (this.another) {
@@ -45,6 +68,11 @@ export default {
       } else {
         this.feedback = "You must enter a value to add an ingredient";
       }
+    },
+    deleteIng(ing) {
+      this.ingredients = this.ingredients.filter(ingredient => {
+        return ingredient != ing
+      })
     }
   }
 };
@@ -64,5 +92,15 @@ export default {
 
 .add-smoothie .field {
   margin: 20px auto;
+  position: relative;
+}
+
+.add-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
 }
 </style>
--- ninja-smoothies/src/components/Index.vue
+++ ninja-smoothies/src/components/Index.vue
@@ -5,11 +5,16 @@
         <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{smoothie.title}}</h2>
         <ul class="ingredients">
-          <li v-for="(ingredient, index) in smoothie.ingredients" :key="index">
-            <span class="chip">{{ingredient}}</span>
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ing}}</span>
           </li>
         </ul>
       </div>
+      <span class="btn-floating btn-large halfway-fab pink">
+        <router-link :to="{ name: 'EditSmoothie', params: { smoothie_slug: smoothie.slug } }">
+          <i class="material-icons edit">edit</i>
+        </router-link>
+      </span>
     </div>
   </div>
 </template>
@@ -27,7 +32,7 @@ export default {
 
   methods: {
     deleteSmoothie(id) {
-      db.collection("smoothies")
+      db.collection('smoothies')
         .doc(id)
         .delete()
         .then(() => {
