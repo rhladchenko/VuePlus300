37f7d61ed6ecb45a122e1789dff0df3f098967ef
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-05-19 20:04:51 +0300

    Update ninja-smoothies l-57

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -25,6 +25,7 @@
 
 <script>
 import db from "@/firebase/init";
+import slugify from 'slugify'
 
 export default {
   name: "EditSmoothie",
@@ -37,18 +38,18 @@ export default {
   },
   methods: {
     EditSmoothie() {
-      if (this.title) {
+      if (this.smoothie.title) {
         this.feedback = null;
-        this.slug = slugify(this.title, {
+        this.smoothie.slug = slugify(this.smoothie.title, {
           replacement: "-",
           remove: /[$*_+~.()'"!\-:@]/g,
           lower: true
         });
-        db.collection("smoothies")
-          .add({
-            title: this.title,
-            ingredients: this.ingredients,
-            slug: this.slug
+        db.collection("smoothies").doc(this.smoothie.id)
+          .update({
+            title: this.smoothie.title,
+            ingredients: this.smoothie.ingredients,
+            slug: this.smoothie.slug
           })
           .then(() => {
             this.$router.push({ name: "Index" });
