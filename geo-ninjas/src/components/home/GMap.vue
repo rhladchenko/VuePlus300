537925b52bf64efdadf9ec1bc16149a26407c2be
rhladchenko <ruslan.hladchenko@cpcs.ws>
2020-05-25 19:43:55 +0300

    Update geo-ninjas

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+import firebase from "firebase";
+import db from "@/firebase/init";
+
 export default {
   name: "GMap",
   data() {
@@ -22,10 +25,69 @@ export default {
         minZoom: 3,
         streetViewControl: false
       });
+      db.collection("users")
+        .get()
+        .then(users => {
+          users.docs.forEach(doc => {
+            let data = doc.data();
+            if (data.geolocation) {
+              let marker = new google.maps.Marker({
+                position: {
+                  lat: data.geolocation.lat,
+                  lng: data.geolocation.lng
+                },
+                map
+              });
+              //add click event to marker
+              marker.addListener("click", () => {
+                this.$router.push({name: 'ViewProfile', params: {id: doc.id}})
+              });
+            }
+          });
+        });
     }
   },
   mounted() {
-    this.renderMap();
+    //get current user
+    let user = firebase.auth().currentUser;
+    // console.log(user);
+
+    //get user geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          this.lat = pos.coords.latitude;
+          this.lng = pos.coords.longitude;
+
+          //find the user record and then update geocoords
+          db.collection("users")
+            .where("user_id", "==", user.uid)
+            .get()
+            .then(snapshot => {
+              snapshot.forEach(doc => {
+                db.collection("users")
+                  .doc(doc.id)
+                  .update({
+                    geolocation: {
+                      lat: pos.coords.latitude,
+                      lng: pos.coords.longitude
+                    }
+                  });
+              });
+            })
+            .then(() => {
+              this.renderMap();
+            });
+        },
+        err => {
+          console.log(err);
+          this.renderMap();
+        },
+        { maximumAge: 60000, timeout: 3000 }
+      );
+    } else {
+      this.renderMap();
+    }
   }
 };
 </script>
--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -1,28 +1,57 @@
 <template>
   <div class="navbar">
     <nav class="deep-purple darken-1">
-        <div class="container">
-            <a href="/" class="brand-logo left">GeoNinjas!</a>
-            <ul class="right">
-                <li><a href="/signup">Singup</a></li>
-                <li><a href="">Login</a></li>
-            </ul>
-        </div>
+      <div class="container">
+        <router-link class="brand-logo left" :to="{name: 'GMap'}">GeoNinjas!</router-link>
+        <ul class="right">
+          <li v-if="!user">
+            <router-link :to="{name: 'Signup'}">Singup</router-link>
+          </li>
+          <li  v-if="!user">
+            <router-link :to="{name: 'Login'}">Login</router-link>
+          </li>
+          <li v-if="user"><a>{{user.email}}</a></li>
+          <li v-if="user">
+            <a @click="logout">Logout</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
-    name: 'Navbar',
-    data() {
-        return {
-            
-        }
-    },
-}
+  name: "Navbar",
+  data() {
+    return {
+      user: null
+    };
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push({ name: "Login" });
+        });
+    }
+  },
+  created() {
+    // let user = firebase.auth().currentUser
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null
+      }
+    });
+  }
+};
 </script>
 
 <style>
-    
 </style>
